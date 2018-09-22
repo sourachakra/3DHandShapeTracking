7c9186355b26b91fbdd5cd1d8baffbb97fe06ccb
sourachakra <sourachakra@gmail.com>
2018-09-21 21:00:52 -0700

    Update GetSilhouettes.m

--- GetSilhouettes.m
+++ GetSilhouettes.m
@@ -12,9 +12,6 @@ for frame_id = 1:1:7627 %7627
         for i = 1:12327
             restvertices(i,:) = verticesinfo(frame_id,i,:);
         end
-
-%         stlwrite(['./stl_files/pose_' num2str(frame_id) '.stl'], facesinfo+1, restvertices);
-%         mkdir(['./orig_silhouettes1/silhouette_' num2str(frame_id)]);
         
         GetSnapsmod_new(frame_id,['./all_stls/' num2str(frame_id) '.stl'],['./orig_silhouettes1/silhouette_' num2str(frame_id)]);
 
