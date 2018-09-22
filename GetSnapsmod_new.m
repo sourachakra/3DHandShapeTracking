c7e9d44afde4950018bc0609583f94f63b0e3c7c
sourachakra <sourachakra@gmail.com>
2018-09-21 21:52:41 -0700

    Update GetSnapsmod_new.m

--- GetSnapsmod_new.m
+++ GetSnapsmod_new.m
@@ -49,52 +49,31 @@ phase_count = 1;
         count = 1;
         
 
-%             for dphi = -cam_angle:cam_angle:5*cam_angle
              for dtheta = 0:-cam_angle:-7*cam_angle
     
-%                     for zoom_factor = 0.75:0.25:1.25
-      
-%                     zoom_factor
-%                     camzoom(zoom_factor);
+
                     camproj('perspective');
                     view([phase_diff_phi phase_diff_theta]);
                     
                     
-%                     campan(dphi,0); 
-%                     cp = campos;
-%                     campos([cp(1)-dphi, cp(2), cp(3)]);
                     
                     
                     camorbit(-(dtheta+diff_theta)/2,0,'camera')
 
                     CurrentImage = getframe(gca);
                     ImageData = CurrentImage.cdata; 
-            %         figure;
-%                     ImageData_binary = ImageData;
-%                     min(min(ImageData))
+
                     ImageData_binary = rgb2gray(ImageData);
-% %                     min(min(ImageData_binary))
-%                     ImageData_binary = imbinarize(ImageData_binary);
-% %                     imshow(ImageData_binary)
-%                     ImageData_binary = imbinarize(ImageData_binary,0.7);
-%                     
+
+                    
                     ImageData_binary(ImageData_binary == ImageData_binary(1,1)) = 0;
                     ImageData_binary(ImageData_binary ~= ImageData_binary(1,1)) = 256;
-%                     for i = 1:size(ImageData,1)
-%                         for j = 1:size(ImageData,2)
-%                             if(ImageData(i,j,:)~= ImageData(1,1))
-%                                 ImageData_binary(i,j,:) = [256 256 256];
-%                             else
-%                                  ImageData_binary(i,j,:) = [0 0 0];
-%                             end
-%                         end
-%                     end
+
 
                     f = flipud(fliplr(ImageData_binary));
-%                     f = ImageData_binary;
-%                     mkdir(['./' output_folder '/' 'silhouette_' num2str((frame_id-1)*9+phase_count)]);
+
                     mkdir(['./binary_silhouettes_newmod12/silhouette_' num2str((frame_id-1)*3+phase_count) '_binary']);
-%                     imwrite(flipud(ImageData),['./' output_folder '/'  'silhouette_' num2str((frame_id-1)*9+phase_count) './' num2str(count) '.png']);
+
                     if(count==1 || count == 4 || count == 6)
                         if count==1 
                             count_mod = 1;
@@ -105,10 +84,8 @@ phase_count = 1;
                         end
                         imwrite(imresize(f,[64 64]),['./binary_silhouettes_newmod12/silhouette_' num2str((frame_id-1)*3+phase_count) '_binary/' num2str(count_mod) '.png']);
                     end
-%                     count                    
+                 
                     count = count + 1;
-                    
-%                     end
 
             end
      
